1c5726e1e150bb2dad39679f4d4c489252c906b7
Daniel Armbrust <darmbrust@gmail.com>
2024-01-15 20:09:31 -0300

    Working with OCI DevOps

--- webapp/docker-entrypoint.sh
+++ webapp/docker-entrypoint.sh
@@ -6,13 +6,13 @@ cd /opt/webapp/motando/
 
 if [ "$APP_ENV" == "PRD" ]; then
    # MySQL - Web Application Password (read the secret value from OCI VAULT)
-    export MYSQL_PASSWD="$(oci --auth resource_principal secrets secret-bundle get \
+   export MYSQL_PASSWD="$(oci --auth resource_principal secrets secret-bundle get \
                                --secret-id "$MYSQL_WEBAPPL_SECRET_OCID" \
                                --stage "LATEST" --query 'data."secret-bundle-content".content' \
                                --raw-output | base64 -d)"
 
-    # OCI - Object Storage Namespace
-    export OCI_OBJSTR_NAMESPACE="$(oci --auth resource_principal os ns get \
+   # OCI - Object Storage Namespace
+   export OCI_OBJSTR_NAMESPACE="$(oci --auth resource_principal os ns get \
                                        --query 'data' --raw-output)"
    
    # Motando - OCI Object Storage ACCESS KEY (read the secret value from OCI VAULT)
@@ -33,8 +33,7 @@ if [ "$APP_ENV" == "PRD" ]; then
                                    --stage "LATEST" --query 'data."secret-bundle-content".content' \
                                    --raw-output | base64 -d)"   
 
-    exec gunicorn motando.wsgi:application --access-logfile - --error-logfile - --bind 0.0.0.0:8000 --workers 2 --threads 2
-
+   exec gunicorn motando.wsgi:application --access-logfile - --error-logfile - --bind 0.0.0.0:8000 --workers 2 --threads 2
 else
    # Dev environment 
    ./motando-webapp-init.sh
